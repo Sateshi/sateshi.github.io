4af06886912116567ffa7a3eadca06ffdb72757f
Sateshi <sateshiplay@gmail.com>
2022-01-03 11:40:22 +0100

    leftText JB

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,19 @@
+let about =  document.getElementById('about');
+let projects = document.getElementById('projectsScroll')
+let leftText = document.getElementById('leftText')
+let body = document.body;
+
+window.addEventListener('scroll',function(e) {
+    console.log("ouais")
+    if(window.scrollY >= about.scrollHeight && window.scrollY < projects.scrollHeight){
+        leftText.innerHTML = 'ABOUT'
+    } else if(window.scrollY >= projects.scrollHeight){
+        leftText.innerHTML = "PROJECTS"
+    } else {
+        leftText.innerHTML = "HOME"
+    }
+})
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
